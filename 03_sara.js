d226016a0f54076292c7330098985661aac0461a
saradipasquale <saradipasquale@seattleacademy.org>
2016-02-09 14:05:25 -0800

    add 03_sara.js

---
+++ 03_sara.js
@@ -0,0 +1,12 @@
+var net = require("net");
+//console.log("net", net);
+
+var server = net.createServer(respond);
+
+function respond(socket){
+	d = newDate();
+	socket.end(d.toString());
+}
+var port = process.argv[2];
+console.log("Listening on port", port);
+server.listen(port);
